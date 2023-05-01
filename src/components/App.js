12ae1bae4ae83b5c486df3c5f878dca727712959
dMuguti <omiahderek56@gmail.com>
2023-05-01 08:16:50 +0300

    commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import BotsData from "./BotsData";
+
+function App() {
+  return (
+    <>
+    
+     <div className="heading">
+      <h4>Bots Collection</h4>
+      <h4>My Bots Army</h4>
+     </div>
+     <BotsData />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BotsData.js
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from "react";
+import BotsCollection from "./BotsCollection";
+import MyBotsArmy from "./MyBotsArmy";
+
+
+function BotsData() {
+    
+    const [bots, setBots] = useState([])
+    const [myBotsArmy, setMyBotsArmy] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:8001/bots")
+        .then(res => res.json())
+        .then(data => {setBots(data)})
+    },[])
+    
+    const botsElement = bots.map(bot => {
+        return <BotsCollection 
+        key={bot.id} 
+        bot={bot}
+        AddMyBot={AddMyBot}
+        deleteBot={deleteBot}/>
+    })
+    
+    function AddMyBot(item) {
+        if (!myBotsArmy.includes(item)) {
+            setMyBotsArmy(prevState => {
+            return [...prevState, item]
+          })
+       }
+    }
+
+    const myBotElement = myBotsArmy.map((bot, index) => {
+        return <MyBotsArmy 
+        key={index}
+        mybot={bot}
+        removeMyBot={removeMyBot} />
+    })
+
+    function removeMyBot(element) {
+
+        setMyBotsArmy(prevState => {
+            return prevState.filter(bot => bot !== element);
+        });
+        
+    }
+
+    function deleteBot(id) {
+        fetch(`http://localhost:8001/bots/${id}`, {
+            method: "DELETE",
+        })
+        //.then((res)=>res.json())
+        .then(() =>{
+            const updatedBots = bots.filter(bot => bot.id !== id)
+            setBots(updatedBots)
+        })
+        console.log('clicked');
+    }
+
+
+
+
+    return (
+        <div className="bots-containers">
+            
+            <div className='bots-collection'>
+            {botsElement}
+            </div>
+            <div className="bots-collection">
+            {myBotElement}
+            </div>
+            
+        </div>
+    )
+}
+
+export default BotsData;
